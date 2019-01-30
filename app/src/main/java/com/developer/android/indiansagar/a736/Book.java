e13e623ca553ed2903386f9197e69debe0508f39
dilip640 <dilipsharma640@gmail.com>
2019-01-30 22:35:36 +0530

    oflline done

--- app/src/main/java/com/developer/android/indiansagar/a736/Book.java
+++ app/src/main/java/com/developer/android/indiansagar/a736/Book.java
@@ -1,8 +1,13 @@
 package com.developer.android.indiansagar.a736;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -16,16 +21,28 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Book extends AppCompatActivity {
 
     PDFView book1;
+    String Book_name;
 
-
+    private ProgressDialog pDialog;
+    public static final int progress_bar_type = 0;
 
     private AdView adView;
     private com.facebook.ads.AdView mAdView;
     private com.google.android.gms.ads.InterstitialAd intad;
     private com.facebook.ads.InterstitialAd interstitialAd;
+
+    private String pathToSave ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +51,117 @@ public class Book extends AppCompatActivity {
         loadFBAds();
 
         book1 = (PDFView) findViewById(R.id.book1);
+
         Intent i = getIntent();
-        book1.fromAsset(i.getStringExtra("book_name")).load();
+        Book_name=i.getStringExtra("book_name");
+        String drive_id=i.getStringExtra("drive_id");
+
+        pathToSave =
+                Environment.getExternalStorageDirectory().toString() + "/." + this.getPackageName()+"/";
+
+        new File(pathToSave).mkdirs();
+
+        if (!new File(pathToSave+Book_name).exists())
+            new DownloadFileFromURL().execute("https://docs.google.com/uc?export=download&id="+drive_id);
+        else
+            book1.fromFile(new File(pathToSave+Book_name)).load();
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case progress_bar_type: // we set this to 0
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Downloading file. Please wait...");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+                return pDialog;
+            default:
+                return null;
+        }
+    }
+
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread
+         * Show Progress Bar Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showDialog(progress_bar_type);
+        }
+
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection conection = url.openConnection();
+                conection.connect();
+                // this will be useful so that you can show a tipical 0-100% progress bar
+                int lenghtOfFile = conection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream(pathToSave+Book_name);
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress(""+(int)((total*100)/lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task
+         * Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            dismissDialog(progress_bar_type);
+            book1.fromFile(new File(pathToSave+Book_name)).load();
+        }
 
     }
 
